54881dcf4071fde5e3502fa864118007dc23803c
Jinwoo Kim <kidjinwoo@me.com>
2023-04-05 23:30:35 +0900

    no message

---
+++ WatchMoji/WatchMojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMojiApp.swift
+//  WatchMoji Watch App
+//
+//  Created by Jinwoo Kim on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchMoji_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
